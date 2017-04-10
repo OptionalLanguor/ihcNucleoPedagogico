5078c23aacbfe1df9ce4375578aba3d42fdc22fe
paulori22 <paulinhoboneti@yahoo.com.br>
2017-04-10 09:25:22 -0300

    Estrutura nova das páginas + mudança de cor do menu
    1- Mudei as estruturas das páginas, agora as coisas em comum como menu, header e scripts estão na pasta nova chamada communs. Desta forma todas as páginas carregam o mesmo menu, header e scripts.
    2- Adiconei o efeito ao menu que muda a cor quando ele esta na paǵina.

--- application/views/cadastrar_palavra_chave.php
+++ application/views/cadastrar_palavra_chave.php
@@ -1,33 +1,15 @@
 <!DOCTYPE html>
 <html>
 <title>Cadastro de Palavras Chave</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-<style>
-body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
-body {font-size:16px;}
-.w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
-.w3-half img:hover{opacity:1}
-</style>
+
+<!-- Header commons/header -->
+<?php $this->load->view('commons/header'); ?>
+
 <body>
 
 <!-- Sidebar/menu -->
 <nav class="w3-sidebar w3-indigo w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:25px">Close Menu</a>
-  <div class="w3-container">
-    <h3 class="w3-padding-64"><b>Universidade Federal de Itajubá<br><h5><i>campus</i> Itabira</b></h5></h3>
-  </div>
-  <div class="w3-bar-block">
-    <a href="#" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Menu Principal</a>
- 
-<a href="#services" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Cadastro de Aluno</a>  
- <a href="#services" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Cadastro de Categoria</a> 
- <a href="#services" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Cadastro de Professor</a>      
- <a href="#showcase" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Registros de Atendimento</a>
-    <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Fale Conosco</a>
-  </div>
+    <?php $this->load->view('commons/menu'); ?>
 </nav>
 
 <!-- Overlay effect when opening sidebar on small screens -->
@@ -41,7 +23,7 @@ body {font-size:16px;}
     <h1 class="w3-xxlarge w3-text-indigo"><b>Cadastro de Palavras Chave</b></h1>
   </div>
 
-     
+
   <!-- Preenchimento dos Campos -->
     <p>Insira a(s) palavra(s) chave(s) no campo abaixo. </p>
     <form action="/action_page.php" target="_blank">
@@ -49,7 +31,7 @@ body {font-size:16px;}
         <label>Palavra Chave</label>
         <input class="w3-input" type="text" name="Name" required>
       </div>
-      
+
       <button type="submit" class="w3-button w3-block w3-indigo w3-margin-bottom">Cadastrar Palavra Chave</button>
     </form>
   </div>
@@ -58,26 +40,8 @@ body {font-size:16px;}
 </div>
 
 
-<script>
-// Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
-
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
-</script>
+<!--Scripts da paǵina -->
+<?php $this->load->view('commons/scripts'); ?>
 
 </body>
 </html>
--- application/views/menu_principal.php
+++ application/views/menu_principal.php
@@ -1,33 +1,15 @@
 <!DOCTYPE html>
 <html>
-<title>W3.CSS Template</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-<style>
-body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
-body {font-size:16px;}
-.w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
-.w3-half img:hover{opacity:1}
-</style>
+<title>Núcleo Pedagogico</title>
+
+<!-- Header commons/header -->
+<?php $this->load->view('commons/header'); ?>
+
 <body>
 
 <!-- Sidebar/menu -->
 <nav class="w3-sidebar w3-indigo w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
-  <div class="w3-container">
-    <h3 class="w3-padding-64"><b>Universidade Federal de Itajubá<br><h5><i>campus</i> Itabira</b></h5></h3>
-  </div>
-  <div class="w3-bar-block">
-    <a href="#" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Menu Principal</a> 
-    <a href="#showcase" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Registros de Atendimento</a> 
-    <a href="#services" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Cadastrar Aluno</a> 
-    <a href="#designers" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Cadastrar Professor</a> 
-    <a href="<?php echo site_url('Cadastrar_palavra_chave') ?>" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Cadastrar Palavra-Chave</a> 
-    <a href="<?php echo site_url('Cadastrar_funcionario') ?>" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Cadastrar Funcionario</a>
-      
-  </div>
+    <?php $this->load->view('commons/menu'); ?>
 </nav>
 <!-- Top menu on small screens -->
 <header class="w3-container w3-top w3-hide-large w3-indigo w3-xlarge w3-padding">
@@ -37,6 +19,7 @@ body {font-size:16px;}
 
 <!-- Overlay effect when opening sidebar on small screens -->
 <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
 
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:340px;margin-right:40px">
@@ -52,7 +35,7 @@ body {font-size:16px;}
 	nec consectetur justo tempus. Fusce congue eu ipsum ac faucibus. Pellentesque at finibus nisl.
     </p>
   </div>
-  
+
   <!-- Services -->
   <div class="w3-container" id="services" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-indigo"><b>Mais atendimentos.</b></h1>
@@ -63,7 +46,7 @@ body {font-size:16px;}
     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     </p>
   </div>
-  
+
   <!-- Designers -->
   <div class="w3-container" id="designers" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-indigo"><b>E muitos outros.</b></h1>
@@ -135,7 +118,7 @@ body {font-size:16px;}
         </li>
       </ul>
     </div>
-        
+
     <div class="w3-half">
       <ul class="w3-ul w3-light-grey w3-center">
         <li class="w3-indigo w3-xlarge w3-padding-32">Pro</li>
@@ -154,7 +137,7 @@ body {font-size:16px;}
       </ul>
     </div>
   </div>
-  
+
   <!-- Contact -->
   <div class="w3-container" id="contact" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-indigo"><b>Contact.</b></h1>
@@ -174,35 +157,18 @@ body {font-size:16px;}
         <input class="w3-input w3-border" type="text" name="Message" required>
       </div>
       <button type="submit" class="w3-button w3-block w3-padding-large w3-red w3-margin-bottom">Send Message</button>
-    </form>  
+    </form>
   </div>
 
 <!-- End page content -->
 </div>
 
-<!-- W3.CSS Container 
+<!-- W3.CSS Container
 <div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px"><p class="w3-right">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></p></div>
 -->
-<script>
-// Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
- 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
 
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
-</script>
+<!--Scripts da paǵina -->
+<?php $this->load->view('commons/scripts'); ?>
 
 </body>
 </html>
