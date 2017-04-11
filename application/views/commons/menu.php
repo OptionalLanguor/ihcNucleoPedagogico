c47570c041ffc79160286070f6ebaee25c5bb7d7
Alexandre <alexandre.nog212@gmail.com>
2017-04-11 01:25:12 -0300

    Correção no Menu
    Em ves de uma tabela com varias linhas, uma tabela para cada linha, para
    não ter as colunas alinhadas.

--- application/views/commons/menu.php
+++ application/views/commons/menu.php
@@ -3,19 +3,23 @@
   <h3 class="w3-padding-64"><b>Universidade Federal de Itajubá<br><h5><i>campus</i> Itabira</b></h5></h3>
 </div>
 <div class="w3-bar-block">
-	<table>
+
 	<!--Tela Principal-->
-	<tr>
+	<table>
+	<td>
   		<a id="menu_principal" href="<?php echo base_url() ?>" onclick="w3_close()" class="<?php if($this->uri->segment(1)==""){echo "w3-bar-item w3-button w3-white";}else{echo "w3-bar-item w3-button w3-hover-white";}?>"><i class="fa fa-home"></i> Menu Principal</a>
-	</tr>
+	</td>
+	</table>
 	<!--Registro de Atendimento-->
-	<tr>
+	<table>
+	<td>
   		<a href="#showcase" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white"><i class="fa fa-calendar-plus-o"></i> Registros de Atendimento</a>
-	</tr>
+	</td>
+	</table>
 	<!--Aluno-->
-	<tr>
+	<table>
 		<td>
-			<i class="fa fa-user-o"></i>
+			<i class="fa fa-graduation-cap"></i>
 			<label>Aluno</label>
 		</td>
 	<td>
@@ -27,18 +31,23 @@
   	<td>
   		<a id="aluno" href="<?php echo site_url('Editar_aluno') ?>" onclick="w3_close()" class="<?php if($this->uri->segment(1)=="Editar_aluno"){echo "w3-bar-item w3-button w3-white";}else{echo "w3-bar-item w3-button w3-hover-white";}?>"><i class="fa fa-pencil"></i></a>
 	</td>
-	</tr>
+	</table>
 	<!--Professor-->
-	<tr>
+	<table>
+	<td>
   		<a href="#designers" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white"><i class="fa fa-drivers-license-o"></i> Cadastrar Professor</a>
-	</tr>
+	</td>
+	</table>
 	<!--Palavra-Chave-->
-	<tr>
+	<table>
+	<td>
   		<a href="<?php echo site_url('Cadastrar_palavra_chave') ?>" onclick="w3_close()" class="<?php if($this->uri->segment(1)=="Cadastrar_palavra_chave"){echo "w3-bar-item w3-button w3-white";}else{echo "w3-bar-item w3-button w3-hover-white";}?>"><i class="fa fa-key"></i> Cadastrar Palavra-Chave</a>
-	</tr>
+	</td>
+	</table>
 	<!--Funcionario-->
-	<tr>
-  		<a href="<?php echo site_url('Cadastrar_funcionario') ?>" onclick="w3_close()" class="<?php if($this->uri->segment(1)=="Cadastrar_funcionario"){echo "w3-bar-item w3-button w3-white";}else{echo "w3-bar-item w3-button w3-hover-white";}?>">Cadastrar Funcionario</a>
-	</tr>
-  </table>
+	<table>
+	<td>
+  		<a href="<?php echo site_url('Cadastrar_funcionario') ?>" onclick="w3_close()" class="<?php if($this->uri->segment(1)=="Cadastrar_funcionario"){echo "w3-bar-item w3-button w3-white";}else{echo "w3-bar-item w3-button w3-hover-white";}?>"><i class="fa fa-user-circle-o"></i> Cadastrar Funcionario</a>
+	</td>
+	</table>
 </div>
