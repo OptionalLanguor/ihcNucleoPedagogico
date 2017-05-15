6d6fa1f934008496dc28c6ef06bc77d883e08f4b
Languor <felmt@hotmail.com>
2017-05-15 16:17:57 -0300

    Correção de typo no view do menu lateral
    Turorial -> Tutorial

--- application/views/commons/menu.php
+++ application/views/commons/menu.php
@@ -62,7 +62,7 @@
 	<table>
 	<td>
   		<a href="<?php echo site_url('Tutorial') ?>" onclick="w3_close()" class="<?php if($this->uri->segment(1)=="Tutorial"){echo "w3-bar-item w3-button w3-white";}else{echo "w3-bar-item w3-button w3-hover-white";}?>"><i class="fa fa-question-circle">
-  		</i> Turorial</a>
+  		</i> Tutorial</a>
 	</td>
 	</table>
 
